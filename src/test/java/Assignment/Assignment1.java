30bbc463e33a0970861ad6da30017425a8247b8b
chitr <chitrakurupati970@gmail.com>
2024-07-15 07:20:04 +0530

    new_login

--- src/test/java/Assignment/Assignment1.java
+++ src/test/java/Assignment/Assignment1.java
@@ -6,9 +6,10 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class Assignment1 {
+    EdgeDriver driver=new EdgeDriver();
+
     @Test
     public void negitve_login() throws InterruptedException {
-        EdgeDriver driver=new EdgeDriver();
         driver.get("https://app.vwo.com/#/login");
         driver.findElement(By.id("login-username")).sendKeys("chitra@gmail.com");
         driver.findElement(By.id("login-password")).sendKeys("admin@123");
@@ -17,5 +18,13 @@ public class Assignment1 {
         Thread.sleep(1000);
         String text=driver.findElement(By.id("js-notification-box-msg")).getText();
         System.out.println(text);
+    }
+    @Test
+    public void new_login()
+    {
+        driver.get("https://app.vwo.com/#/login");
+        driver.findElement(By.className("text-link")).click();
+        driver.findElement(By.name("email")).sendKeys("chitra@123");
+        driver.findElement(By.name("gdpr_consent_checkbox")).click();
     }
 }
